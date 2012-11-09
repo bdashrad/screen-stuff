54f8319dd5cc9e8c5157c81d617ae420dce48f8c
Brad Clark <bdashrad@itd-bclark.plumbedford.local>
2012-11-09 17:37:38 -0500

    irssi notification script

---
+++ irssi_notify.sh
@@ -0,0 +1,16 @@
+# yes, we need a way to flush the file on disconnect; i don't know one
+# yes, that's flush is not atomic (but good enough for me)
+ssh shell.bdashrad.com "tail -n 10 /home/ec2-user/.irssi/fnotify; > /home/ec2-user/.irssi/fnotify; tail -f /home/ec2-user/.irssi/fnotify" |
+#	sed -u 's/[<@&]//g' |
+	sed -ru "
+		s/&/\&amp;/g;
+		s/</\&lt;/g;
+		s/>/\&gt;/g;
+		s%(#[a-zA-Z]+ )&lt;([^>]{0,10})&gt;%\1<b>\2</b> %g
+		s%(https?://[^ ]+\.[a-zA-Z]{1,3})%<a href=\"\1\">\1</a>%g;
+		s%([a-zA-Z0-9+_-]+@[^ ]+\.[a-zA-Z]{1,3})%<a href=\"mailto:\1\">\1</a>%g;" |
+	while read heading message
+	do
+		notify-send -i gtk-dialog-info -t 300000 -- "${heading}" "${message}"
+	done
+# the sed -u 's/[<@&]//g' is needed as those characters might confuse  notify-send (FIXME: is that a bug or a feature?)
